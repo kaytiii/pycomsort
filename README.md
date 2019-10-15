3f20a57469b29f9f2688e78df3c5ae3b4262d8b8
Kayti <kak240@musc.edu>
2019-10-15 15:50:17 -0400

    intial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+pycomsort readme
+
+
---
+++ pycomsort.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 15 15:36:52 2019
+
+@author: kayti
+"""
+
+import dicom
+ds = dicom.read_file("1111.img")
