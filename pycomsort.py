77f894bf9453acb4e0e9cd9b981866c7413bbf7a
Kayti <kak240@musc.edu>
2022-01-11 12:17:43 -0500

    Updates pycomsort

--- pycomsort.py
+++ pycomsort.py
@@ -2,32 +2,40 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Oct 15 15:36:52 2019
-
-@author: kayti
+@author: kayti thorn
 """
-
 import pydicom
 import os 
+import glob
+import os.path
 
-path = os.getcwd()
-#path='/Users/kayti/Desktop/Projects/Tests/pycomsort_test'
+#directory = os.getcwd()
 
-subjids = os.listdir(path)
-subjids.remove('.DS_Store')
+directory = "/Users/kayti/Desktop/Projects/pycomsort_dev/test_data/"
+subjids = os.listdir(directory)
+if '.DS_Store' in subjids:
+  subjids.remove('.DS_Store')
 
-for ids in subjids:
-    dcms = os.listdir(path + '/' + ids + '/dicom')
-    if '.DS_Store' in dcms:
-        dcms.remove('.DS_Store')
-    for dcm in dcms:
-        dcmhdr = pydicom.dcmread(path + '/' + ids + '/dicom/' + dcm)
-        seq = dcmhdr.ProtocolName
-        if os.path.exists(path + '/' + ids + '/dicom/' + seq):
-            oldfile=os.path.join(path + '/' + ids + '/dicom/' + dcm)
-            newfile=os.path.join(path + '/' + ids + '/dicom/' + seq + '/' + dcm)
-            os.rename(oldfile,newfile)
-        else: 
-            os.mkdir(path + '/' + ids + '/dicom/' + seq)
-            oldfile=os.path.join(path + '/' + ids + '/dicom/' + dcm)
-            newfile=os.path.join(path + '/' + ids + '/dicom/' + seq + '/' + dcm)
-            os.rename(oldfile,newfile)
+for ids in subjids:  
+  path = directory + ids + "/**/*"
+  dcmspath = glob.glob(path, recursive=True)
+  dcmspath2=dcmspath[100]
+  dcmpath = os.path.dirname(dcmspath2)
+  dcms = os.listdir(dcmpath)
+  if '.DS_Store' in dcms:
+      dcms.remove('.DS_Store')
+  for dcm in dcms:
+      dcmhdr = pydicom.dcmread(dcmpath + '/' + dcm)
+      seq = dcmhdr.ProtocolName
+      if os.path.exists(directory + '/' + ids + '/' + seq):
+          oldfile=os.path.join(dcmpath + '/' + dcm)
+          newfile=os.path.join(directory + '/' + ids + '/' + seq + '/' + dcm)
+          os.rename(oldfile,newfile)
+      else: 
+          os.mkdir(directory + '/' + ids + '/' + seq)
+          oldfile=os.path.join(dcmpath + '/' + dcm)
+          newfile=os.path.join(directory + '/' + ids + '/' + seq + '/' + dcm)
+          os.rename(oldfile,newfile)
+      for entry in os.scandir(directory + '/' + ids):
+        if os.path.isdir(entry.path) and not os.listdir(entry.path) :
+          os.rmdir(entry.path)
